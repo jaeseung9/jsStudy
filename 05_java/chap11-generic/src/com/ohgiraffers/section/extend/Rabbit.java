eb6daf319752c30016493a07526f9acfa175019a
jaeseung9 <seojaeseung9@gmail.com>
2025-11-05 18:26:48 +0900

    11.05 수업종료

---
+++ 05_java/chap11-generic/src/com/ohgiraffers/section/extend/Rabbit.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section.extend;
+
+public class Rabbit extends Mammal {
+
+    public void cry(){
+        System.out.println("토끼까 울음 소리를 낸다.");
+    }
+}
---
+++ 05_java/chap11-generic/src/com/ohgiraffers/section/extend/Reptile.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.section.extend;
+
+public class Reptile {
+}
